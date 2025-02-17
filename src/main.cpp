2fa1b31484d18f97c3b652694209232710ffb959
Junichi Akita <akita@ifdl.jp>
2025-02-17 20:15:31 +0900

    TCP&UDP config

--- src/main.cpp
+++ src/main.cpp
@@ -2,69 +2,109 @@
 #include <WiFi.h>
 #include <WiFiUdp.h>
 
-//#define SERVER // for UDP Server
+#define SERVER // for UDP Server
+
+#define TCP // use TCP, instead of UDP
 
 const char *ssid = "DualAccUDP_AP";
 const char *password = "12345678";
 
+#define PORT 12345
+ #ifdef TCP
+WiFiServer server(PORT);
+WiFiClient client;
+ #else // UDP
 WiFiUDP udp;
-const int UDPPort = 12345;
+ #endif
 const char *serverIP = "192.168.4.1"; // APのIP
+char buf[1025];
+
 #ifdef SERVER
-char buf[1024];
+uint32_t t0;
 #else
+int n = 0;
 #endif
 
 void setup()
 {
 	M5.begin();
-	Serial.begin(115200);
 
 #ifdef SERVER
 	// アクセスポイントモードに設定
 	WiFi.softAP(ssid, password);
-	Serial.println("WiFi AP started");
-	Serial.print("AP IP Address: ");
-	Serial.println(WiFi.softAPIP());
-	// UDP受信開始
-	udp.begin(UDPPort);
-	Serial.printf("Listening on UDP port %d\n", UDPPort);
-#else
+	printf("WiFi AP started, IP Address: %s\n", WiFi.softAPIP().toString());
+ #ifdef TCP
+	server.begin();
+	printf("TCP server started\n");
+ #else // UDP
+	udp.begin(PORT);
+	printf("Listening on UDP port %d\n", PORT);
+ #endif
+	t0 = millis();
+#else // not SERVER
 	WiFi.begin(ssid, password);
-	Serial.print("Connecting to WiFi...");
+	printf("Connecting to WiFi...");
 	while (WiFi.status() != WL_CONNECTED)
 	{
 		delay(500);
-		Serial.print(".");
+		printf(".");
 	}
-
-	Serial.println("\nConnected to WiFi!");
-	Serial.print("IP Address: ");
-	Serial.println(WiFi.localIP());
+	printf("\nConnected to WiFi!, IP Address: %s\n", WiFi.localIP().toString());
+	for (uint16_t i = 0; i < 1000; i++) buf[i] = '0' + (i % 10);
+	buf[1000] = '\0';
+ #ifdef TCP
+ 	printf("Connecting to TCP server...");
+ 	while (!client.connect(serverIP, PORT)) {
+		printf(".");
+		delay(1000);
+	}
+	printf("\nConnected to TCP server\n");
+ #endif
 #endif
 }
 
 void loop()
 {
 #ifdef SERVER
+ #ifdef TCP
+	if (!client) {
+		client = server.available();  // クライアントの接続を待機
+		if (client) printf("Client connected.\n");
+	} else {
+		if (client.available()) {
+			int len = client.available();
+			if (len > 1024) len = 1024;
+			int bytesRead = client.readBytes(buf, len);
+			printf("%d,%d\n", millis() - t0, bytesRead);
+	}
+}
+
+ #else // UDP
 	int packetSize = udp.parsePacket();
 	if (packetSize)
 	{
-		int len = udp.read(buf, 1023);
+		int len = udp.read(buf, 1024);
 		if (len > 0)
 		{
 			buf[len] = '\0';
 		}
-		Serial.printf("Received packet: %s\n", buf);
+		printf("%d,%d\n", millis() - t0, len);
 	}
-#else
-	const char *message = "Hello from M5Stack_B!";
-	udp.beginPacket(serverIP, UDPPort);
-	udp.print(message);
+ #endif
+#else // not SERVER
+	buf[0] = 'A' + (n % 10); n++; buf[1] = '1';
+ #ifdef TCP
+ 	if (client.connected()) {
+		client.print(buf);
+	}
+	else {
+		printf("Disconnected from server, attempting to reconnect...\n");
+		client.connect(serverIP, PORT);
+	}
+ #else // UDP
+	udp.beginPacket(serverIP, PORT);
+	udp.print(buf);
 	udp.endPacket();
-
-	Serial.println("Sent packet: " + String(message));
-
-	delay(1000); // 1秒ごとに送信
+ #endif
 #endif
 }
